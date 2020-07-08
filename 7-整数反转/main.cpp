5a1a4dfd739e18a5d0157b9ae6b748fbe340be3f
ltqthu <1355583269@qq.com>
2020-07-08 21:31:57 +0800

    init

---
+++ 7-整数反转/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// 123
+// 123 % 10 = 3, 123/10 = 12,3
+class Solution {
+public:
+    int reverse(int x) {
+        bool positive = (x>=0) ? true : false;
+        if (!positive) x = -x;
+
+        int reverseInt = 0;
+        int rem;
+        while (x != 0) {
+            rem = x % 10;
+            x = x/10;
+            reverseInt = 10 * reverseInt + rem;
+        }
+        return positive?reverseInt:-reverseInt;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    int x1 = 123;
+    cout << s.reverse(x1) << endl;
+    int x2 = -234;
+    cout << s.reverse(x2) << endl;
+    int x3 = 0;
+    cout << s.reverse(x3) << endl;
+    cout << INT_MAX << endl;
+
+    return 0;
+}
---
+++ sword-to-offer/offer3_solution1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for ( int i=1; i<nums.size(); i++ ) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> v1{2, 3, 1, 0, 2, 5, 3};
+    Solution s = Solution();
+    cout << s.findRepeatNumber(v1) << endl;
+    return 0;
+}
