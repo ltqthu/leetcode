c58d0acef5b68b46aa8e4a0f9d133e0926d6ba5a
liutq <1355583269@qq.com>
2020-09-07 01:46:44 +0800

    add leetcode8

--- 13-罗马数字转整数/leetcode13_solution1.cpp
+++ 13-罗马数字转整数/leetcode13_solution1.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char, int> roman_map{{'I',1}, {'V',5}, {'X',10}, {'L',50},
-                                 {'C',100}, {'D',500}, {'M',1000}};
+        map<char, int> roman_map{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int sum = 0;
-        for (int i = 0; i < s.size()-1; ++i) {
-            if (roman_map[s[i]] < roman_map[s[i+1]]) {
+        for (int i = 0; i < s.size() - 1; ++i) {
+            if (roman_map[s[i]] < roman_map[s[i + 1]]) {
                 sum -= roman_map[s[i]];
             } else {
                 sum += roman_map[s[i]];
@@ -21,14 +20,12 @@ public:
     }
 };
 
-
 int main() {
     Solution s = Solution();
     cout << s.romanToInt("III") << endl;
 
     cout << s.romanToInt("IV") << endl;
     cout << s.romanToInt("LVIII") << endl;
-
 
     return 0;
 }
